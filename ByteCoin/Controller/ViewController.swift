c116f212bf64a393f3079d6ac16bd7f7e557f799
Gustavo Matoso <117685566+GustavoMatoso@users.noreply.github.com>
2023-04-11 15:59:15 -0300

    .

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    let coinManager = CoinManager()
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        
+        pickerView.selectRow(coinManager.currencyArray.firstIndex(of: "USD")!, inComponent: 0, animated: false)
+            }
+}
+//MARK: - UIPickerViewDataSource
+    
+extension ViewController: UIPickerViewDataSource{
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        coinManager.currencyArray.count
+    }
 }
 
+//MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate{
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        let selectedCurrency = coinManager.currencyArray[row]
+        
+        currencyLabel.text = selectedCurrency
+        
+        print("Currency selected: \(selectedCurrency)")
+        
+        coinManager.getURL(currency: selectedCurrency)
+    }
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,41 @@
 
 import Foundation
 
+protocol CoinManagerDelegate{
+    func didFailWithError(error: Error)
+}
+
+
+
 struct CoinManager {
     
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC/"
+    let apiKey = "BB05BE43-F373-412F-8E71-A981ECC8EC55"
+    
+    var delegate: CoinManagerDelegate?
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    
+    func getURL(currency: String){
+    
+        let urlString = "\(baseURL)\(currency)?apikey=\(apiKey)"
+        performRequest(with: urlString)
+        
+    }
+    
+    func performRequest(with urlString: String){
+        
+        let url = URL(string: urlString)
+        let session = URLSession(configuration: .default)
+        
+        let task = session.dataTask(with: url!) { data, response, error in
+            if error != nil{
+                delegate?.didFailWithError(error: error!)
+            }
+        }
+        
+    }
+    
     
 }
